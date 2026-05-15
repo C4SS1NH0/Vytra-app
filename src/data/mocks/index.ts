15141d8bf96faf3f6477726ff73ae3359e69a502
Cássio <cassiokk13@gmail.com>
2026-05-15 13:56:29 -0300

    chore: organiza estrutura de mocks e repositories

--- src/data/mocks/index.ts
+++ src/data/mocks/index.ts
@@ -1,6 +1,7 @@
 export { challengesMock } from '@/src/data/mocks/challenges';
 export { eventsMock } from '@/src/data/mocks/events';
 export { postsMock } from '@/src/data/mocks/posts';
+export { eloMock } from '@/src/data/mocks/profile';
 export { runsMock } from '@/src/data/mocks/runs';
 export { currentUserMock, weeklyUsersMock } from '@/src/data/mocks/users';
 export { worldCommunityStatsMock, worldPinsMock, worldWeeklyRunsMock } from '@/src/data/mocks/world';
---
+++ src/data/repositories/index.ts
@@ -0,0 +1,4 @@
+export { MockHomeRepository } from '@/src/data/repositories/MockHomeRepository';
+export { MockProfileRepository } from '@/src/data/repositories/MockProfileRepository';
+export { MockSocialRepository } from '@/src/data/repositories/MockSocialRepository';
+export { MockWorldRepository } from '@/src/data/repositories/MockWorldRepository';
